9a1a74917bf104bd71c3655925212f82c80ef4fe
wushuang <wushuang@gaoding.com>
2022-03-22 14:03:06 +0800

    feat: 支持axios代理接口请求

--- packages/sdk/vite.config.js
+++ packages/sdk/vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'lib/index.ts'),
       name: packages.name,
-      // fileName: (format) => `${packages.name}.${format}.js`,
+      fileName: (format) => `${packages.name}.${format}.js`,
       formats: ['es', 'umd'],
     },
     minify: 'terser' // 混淆器
