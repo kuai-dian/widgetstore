312dc5c153bd034bfcfacbc08fe4faaf618c439a
wushuang <wushuang@gaoding.com>
2022-03-22 13:44:20 +0800

    feat: 支持axios代理接口请求

--- packages/sdk/dist/lib/api.d.ts
+++ packages/sdk/dist/lib/api.d.ts
@@ -8,4 +8,9 @@ export declare const update: (props: any) => void;
  * @param props {any} 参数
  */
 export declare const get: () => Promise<unknown>;
+/**
+ * 代理axios请求方法
+ * @param config {any} axios配置
+ */
+export declare const axios: (config?: any) => Promise<unknown>;
 //# sourceMappingURL=api.d.ts.map
