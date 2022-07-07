65f51c2e32789b1ddd8d5aba3c4dd9408be6ae64
wushuang <wushuang@gaoding.com>
2022-07-07 15:01:46 +0800

    fix: sdk dist

--- packages/sdk/dist/lib/sdk.d.ts
+++ packages/sdk/dist/lib/sdk.d.ts
@@ -2,14 +2,29 @@ export interface IRenderProps {
     options: object;
     data: object;
 }
+/**
+ * 渲染函数
+ */
 declare type IRender = (IRenderProps: IRenderProps) => void;
 /**
  * 定义渲染方法
- * @param render {IRender} 渲染函数
- * @param isRenderingNow {boolean} 是否立即渲染，建议生产不使用，开发阶段将其设置为true
- * @param isSingle {boolean} 是否首次渲染和二次更新同方法，默认分离方法
  */
-export declare const defineRender: (render: IRender, isRenderingNow?: boolean, isSingle?: boolean) => void;
+interface IDefineRenderOptions {
+    /**
+     * 是否为开发模式，立即渲染，开发阶段将其设置为true，一般传递 `process.env.NODE_ENV === 'development'`
+     */
+    isDevMode?: boolean;
+    /**
+     * 是否首次渲染和二次更新同方法，默认分离方法
+     */
+    isSingle?: boolean;
+}
+/**
+ * 定义渲染方法
+ * @param render {IRender} 渲染函数
+ * @param options {IDefineRenderOptions} 参数
+ */
+export declare const defineRender: (render: IRender, options: IDefineRenderOptions) => void;
 /**
  * 定义更新方法
  * @param update {IRender} 更新函数
